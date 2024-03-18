9f026fd44e41dabc4df72d26fa9c71ca593f60fe
Webby1015 <vedantabhishek@gmail.com>
2024-03-18 15:25:27 +0530

    eheheheh

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,101 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Terms from "./Pages/Terms";
 import Policy from "./Pages/Policy";
 import Error from "./Pages/Error";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./config/firebase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log(auth.currentUser.email);
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await auth.signOut();
+      setIsLoggedIn(false);
+      console.log("User logged out");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/policy" element={<Policy />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
+      {isLoggedIn ? (
+        <Router>
+          <button
+            onClick={logout}
+            className="bg-red-500 mx-20 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Logout
+          </button>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/policy" element={<Policy />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h2 className="text-2xl font-bold mb-6">Login</h2>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Enter your username"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 mx-auto hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={login}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
+
 export default App;
--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,22 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getStorage} from "firebase/storage"
+import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRuQIidrNS4OLq3bAGzXF6qHZXocYvxqI",
-  authDomain: "prakhar-portfolio-ce872.firebaseapp.com",
-  projectId: "prakhar-portfolio-ce872",
-  storageBucket: "prakhar-portfolio-ce872.appspot.com",
-  messagingSenderId: "741567686623",
-  appId: "1:741567686623:web:3f74c94875a7517e42466a",
-  measurementId: "G-6TNV37C9JL"
+  apiKey: "AIzaSyDaE1vWJQXpEn8xU_vCRAmKkKCA-rtAQes",
+  authDomain: "p-portfolio-d64b1.firebaseapp.com",
+  projectId: "p-portfolio-d64b1",
+  storageBucket: "p-portfolio-d64b1.appspot.com",
+  messagingSenderId: "985895826934",
+  appId: "1:985895826934:web:501b5e81bb380160b343d7",
+  measurementId: "G-MJEF0CDRFH"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app)
